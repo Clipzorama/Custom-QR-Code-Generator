eb06829748e66917c69e9478fdd3ec910f282790
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-01-21 22:01:47 -0500

    updated index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const generateBtn = document.querySelector(".generate");
+const newqrBtn = document.querySelector(".newQR");
+
+const qrInput = document.getElementById("qrinput");
+let imageContainer = document.querySelector(".img-container");
+const QRimage = document.getElementById("qrimage");
+
+
+generateBtn.addEventListener("click", () => {
+    imageContainer.classList.remove("hidden");
+    generateBtn.classList.add("hidden");
+    newqrBtn.classList.remove("hidden");
+
+    QRimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=180x180&data=" + qrInput.value;
+})
+
+
+newqrBtn.addEventListener("click", () => {
+    imageContainer.classList.add("hidden");
+    qrInput.value = "";
+    newqrBtn.classList.add("hidden");
+    generateBtn.classList.remove("hidden");
+
+
+})
